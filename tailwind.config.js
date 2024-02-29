01889509e20c69b4570ed71c72f5725e21991229
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-29 13:07:53 +0100

    added configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         protestStrike: ['Protest Strike', 'sans-serif'],
       },
+      screens: {
+        xs: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
     },
   },
   plugins: [],
