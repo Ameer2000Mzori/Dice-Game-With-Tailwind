f2562d7e9c33bf1d77dd4d5af2b388e1cb16eec0
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-29 13:59:04 +0100

    added logic showing player their current scores and all time scores  + users can restart game and hold + if user gets number one then it will be the turn of the other user

--- src/script.js
+++ src/script.js
@@ -8,19 +8,36 @@ const holdCurrentP = document.getElementById('hold-Current-P')
 // // elements
 const diceImg = document.getElementsByClassName('dice-Img')[0]
 
+// scores elements
+const playerOneCurrentScore = document.getElementsByClassName(
+  'player-1-current-score'
+)[0]
+const playerTwOCurrentScore = document.getElementsByClassName(
+  'player-2-current-score'
+)[0]
+
+// scores elements all time
+const playerOneScoresAllTime = document.getElementsByClassName(
+  'player-One-Scores-All-Time'
+)[0]
+const playerTwoScoresAllTime = document.getElementsByClassName(
+  'player-Two-Scores-All-Time'
+)[0]
+
 // global variables
 let userOneScore = 0
 let userTwoScore = 0
 let currentScore = 0
 let userOnePlaying = true
 
+// rollDice function
 const rollDice = () => {
   const dice = Math.floor(Math.random() * 6) + 1
 
-  console.log('dice', dice)
   if (dice === 1) {
     currentScore = 0
-
+    playerOneCurrentScore.textContent = currentScore
+    playerTwOCurrentScore.textContent = currentScore
     if (userOnePlaying) {
       userOnePlaying = false
     } else {
@@ -32,19 +49,55 @@ const rollDice = () => {
   diceImg.src = `./assets/dice-${dice}.png`
 
   if (userOnePlaying === true) {
-    console.log(dice)
     currentScore += dice
+    playerOneCurrentScore.textContent = currentScore
     console.log('this is current score', currentScore)
     console.log('user one playing', userOnePlaying)
+    console.log('user one scores', userOneScore)
   }
   if (!userOnePlaying) {
-    console.log(dice)
     currentScore += dice
+    playerTwOCurrentScore.textContent = currentScore
     console.log('this is current score', currentScore)
     console.log('user Two playing', userOnePlaying)
+    console.log('user Two scores', userTwoScore)
   }
 }
 
+// hold current users scores
+const holdUserScores = () => {
+  if (userOnePlaying) {
+    userOneScore += currentScore
+    playerOneScoresAllTime.textContent = userOneScore
+    userOnePlaying = false
+    playerOneCurrentScore.textContent = 0
+  } else {
+    userTwoScore += currentScore
+    playerTwoScoresAllTime.textContent = userTwoScore
+    userOnePlaying = true
+    playerTwOCurrentScore.textContent = 0
+  }
+  currentScore = 0
+}
+
+const restGame = () => {
+  userOneScore = 0
+  userTwoScore = 0
+  currentScore = 0
+  playerOneCurrentScore.textContent = 0
+  playerTwOCurrentScore.textContent = 0
+  playerOneScoresAllTime.textContent = 0
+  playerTwoScoresAllTime.textContent = 0
+  userOnePlaying = true
+}
+
+holdCurrentP.addEventListener('click', () => {
+  holdUserScores()
+})
 rollDiceBtn.addEventListener('click', () => {
   rollDice()
+})
+
+restartGame.addEventListener('click', () => {
+  restGame()
 })
